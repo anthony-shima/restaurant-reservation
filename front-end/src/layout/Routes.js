deed258737cdcf7d499aeb32253e1e43359c6c61
Anthony <shima.anthony@gmail.com>
2024-08-26 08:32:39 -0700

    Update Routes.js

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -1,57 +1,72 @@
 import React from "react";
-
 import { Redirect, Route, Switch } from "react-router-dom";
-import Dashboard from "../dashboard/Dashboard";
-import NotFound from "./NotFound";
+
+// utils
 import { today } from "../utils/date-time";
 import useQuery from "../utils/useQuery";
-import Reservations from "../reservations/NewReservations";
-import Tables from "../tables/tables";
-import Seat from "../seat/seat";
-import Search from "../search/search";
-import Edit from "../reservations/editReservation";
 
+// Route Imports
+import CreateReservation from "../reservations/CreateReservation";
+import CreateTable from "../tables/CreateTable";
+import SeatTable from "../components/SeatTable";
+import Search from "../dashboard/Search";
+import Dashboard from "../dashboard/Dashboard";
+import EditReservation from "../reservations/EditReservation";
+import ReservationStatus from "../reservations/ReservationStatus";
+
+// error handler
+import NotFound from "./NotFound";
 /**
  * Defines all the routes for the application.
  *
  * You will need to make changes to this file.
  *
  * @returns {JSX.Element}
+
  */
-function Routes() {
+export default function Routes() {
   const query = useQuery();
   const date = query.get("date");
+//console.log("routes.js", date)
   return (
     <Switch>
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
-      <Route exact path="/reservations/:reservation_id/seat">
-        <Seat />
-      </Route>
-      <Route exact path="/reservations/:reservation_id/edit">
-        <Edit />
-      </Route>
-      <Route exact path="/reservations/new">
-        <Reservations />
-      </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
       </Route>
+      <Route path="/reservations/new">
+        <CreateReservation />
+      </Route>
+      <Route path="/reservations/:reservation_id/seat">
+        <SeatTable />
+      </Route>
+      <Route path="/reservations/:reservation_id/status">
+        <ReservationStatus />
+      </Route>
+      <Route path="/reservations/:reservation_id/edit">
+        <EditReservation />
+      </Route>
+
+      <Route exact={true} path="/tables">
+        <Redirect to={"/dashboard"} />
+      </Route>
+      <Route path="/tables/new">
+        <CreateTable />
+      </Route>
+
       <Route path="/dashboard">
         <Dashboard date={date || today()} />
       </Route>
-      <Route path="/tables/new">
-        <Tables />
-      </Route>
-      <Route path="/search">
+
+      <Route exact={true} path="/search">
         <Search />
       </Route>
+
       <Route>
         <NotFound />
       </Route>
     </Switch>
   );
 }
-
-export default Routes;
