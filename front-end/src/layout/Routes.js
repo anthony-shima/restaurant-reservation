6606156983de65f659b21c243606840e29651d29
Anthony <shima.anthony@gmail.com>
2024-08-27 08:48:58 -0700

    Update Routes.js

--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -5,6 +5,7 @@ import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
 import useQuery from "../utils/useQuery";
+import Reservations from "../reservations/NewReservations";
 import Tables from "../tables/tables";
 import Seat from "../seat/seat";
 import Search from "../search/search";
